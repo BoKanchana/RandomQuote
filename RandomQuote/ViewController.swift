e20cef439ad6a603bafc71f44340bab44e9f3d9c
Kanchana Phakdeedorn <kanchana.pdk@gmail.com>
2019-08-08 14:15:44 +0700

    Initial Commit

---
+++ RandomQuote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomQuote
+//
+//  Created by Kanchana Phakdeedorn on 8/8/2562 BE.
+//  Copyright © 2562 bobo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
